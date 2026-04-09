b414c53bcc34b25a866383638a4482d1852c1c5d
Lotanna <tannaloamobi@gmail.com>
2026-04-09 16:23:20 +0100

    Add if/else-if/else statement to practice conditional logic with &&

--- src/javaMasterClass/ExpressionsPlayground.java
+++ src/javaMasterClass/ExpressionsPlayground.java
@@ -55,5 +55,19 @@ public class ExpressionsPlayground {
         if (!isNoRemainder) {
             System.out.println("There is a remainder");
         }
+
+        System.out.println("-------------");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
     }
 }
