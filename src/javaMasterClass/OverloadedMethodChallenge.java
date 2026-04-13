b038340b9064b7690b9d42e48f6479561adae475
Lotanna <tannaloamobi@gmail.com>
2026-04-13 21:47:11 +0100

    Add OverloadedMethodChallenge class to practice overloaded method signatures and calls

---
+++ src/javaMasterClass/OverloadedMethodChallenge.java
@@ -0,0 +1,21 @@
+package javaMasterClass;
+
+public class OverloadedMethodChallenge {
+
+    public static void main(String[] args) {
+
+        System.out.println("5Ft 11in = " + convertToCentimeters(5,11 ) + "cm.");
+    }
+
+    public static double convertToCentimeters(int inches) {
+
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+        return convertToCentimeters(totalInches);
+    }
+}
