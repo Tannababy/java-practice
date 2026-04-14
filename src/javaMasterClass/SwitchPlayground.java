690a0694c2c18818c16061a38674a1c080e6c4c4
Lotanna <tannaloamobi@gmail.com>
2026-04-14 17:39:28 +0100

    Add SwitchPlayground class to practice enhanced switch statements

---
+++ src/javaMasterClass/SwitchPlayground.java
@@ -0,0 +1,26 @@
+package javaMasterClass;
+
+public class SwitchPlayground {
+    public static void main(String[] args) {
+
+//        int value = 6;
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+        switch (switchValue){
+            case 1-> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Was a 3, a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
+        }
+    }
+}
