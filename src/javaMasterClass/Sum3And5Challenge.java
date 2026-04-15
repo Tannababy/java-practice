1f54d960cae424bbf541cf6f5a03f001a0a104c1
Lotanna <tannaloamobi@gmail.com>
2026-04-15 13:42:44 +0100

    Add SumOf3And5Challenge class to practice for loops using the debugger tool at different breakpoints

---
+++ src/javaMasterClass/Sum3And5Challenge.java
@@ -0,0 +1,23 @@
+package javaMasterClass;
+
+public class Sum3And5Challenge {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int count = 0;
+        for (int i = 1; i <= 1000; i++){
+
+            if ((i % 3 == 0) && (i % 5 == 0)){
+                sum += i;
+                count++;
+                System.out.println(i + " is divisible by 3 and 5");
+            }
+            if (count == 5){
+                break;
+            }
+
+        }
+        System.out.println("Sum of matches = " + sum);
+    }
+}
