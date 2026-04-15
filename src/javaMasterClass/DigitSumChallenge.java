9c04a43a4d353be2674b9df5f5e13e1ea6c4296e
Lotanna <tannaloamobi@gmail.com>
2026-04-15 16:37:35 +0100

    Add DigitSumChallenge class to further understand while loops

---
+++ src/javaMasterClass/DigitSumChallenge.java
@@ -0,0 +1,30 @@
+package javaMasterClass;
+
+public class DigitSumChallenge {
+
+    public static void main(String[] args) {
+
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(1234));
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(-1234));
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(1000));
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(126));
+    }
+
+    public static int sumDigits(int number){
+
+        if (number < 0){
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 9){ 
+
+           sum += number % 10; // to extract outermost digit and add to sum
+           number /= 10; // to reduce digit position each time
+
+        }
+        sum += number; // to still add number to sum even if it is a single digit number
+        return sum;
+    }
+}
